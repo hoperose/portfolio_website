afb331aa9e4f275e4de6523994aacfd802296b97
Qian Yu <qyhoperose@gmail.com>
2018-08-25 22:46:44 -0700

    first commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+/*var colors = ['blue', 'green', 'red', 'purple', 'yellow'];
+
+$('.card-body').mouseover(function(){
+    var change = colors[Math.floor(Math.random() * colors.length)];
+    $(this).css('background', change);
+});
+
+$('.card-body').mouseleave(function() {
+    $(this).css('background', '');
+});*/
